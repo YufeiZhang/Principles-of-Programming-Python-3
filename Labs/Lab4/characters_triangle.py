7312120752e0cf57556aebb6552f92e1c7a4992b
Yufei Zhang <yufeiGitHub@gmail.com>
2016-08-30 14:09:06 +1000

    Format for q2 is not good enough

--- Labs/Lab4/characters_triangle.py
+++ Labs/Lab4/characters_triangle.py
@@ -1,8 +1,6 @@
 # Write a program characters_triangle.py that gets a strictly positive integer N
 # as input and outputs a triangle of height N, following this kind of interaction:
 
-from math import ceil
-
 try:
 	n = int(input("Enter strictly positive number: "))
 	if n < 0:
@@ -17,7 +15,6 @@ try:
 				print(chr(((m + i+i - j)%26) + 65), end = "")
 			else:
 				print(chr(((m + j)%26) + 65), end = "")
-		print("  "* (n-i), end = "")
 		print()
 
 except ValueError:
