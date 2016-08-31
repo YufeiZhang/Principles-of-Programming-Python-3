4b0796837bdec3e9ca7cb4e3aaf6e4aaf04fcddd
Yufei Zhang <yufeiGitHub@gmail.com>
2016-08-31 20:25:13 +1000

    WTF……Martin, please help me

--- Labs/Lab5/most_popular_name.py
+++ Labs/Lab5/most_popular_name.py
@@ -1,1 +1,13 @@
-most_popular_name.py
+import os.path
+import sys
+import io
+
+for filename in os.listdir('names/'):
+	#print(filename)
+	with io.open('names/' + filename, encoding='utf-8') as ori_txt:
+		print(ori_txt)
+		#print(ori_txt)
+		#for line in ori_txt.encoding:
+		#	print(line.encoding)
+	#txt = open('names/' + filename)
+	#print(txt)
