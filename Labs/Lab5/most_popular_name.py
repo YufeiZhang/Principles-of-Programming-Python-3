39c6a09edc1c22ab8dd8f21b56d700374e7d1d9d
Yufei Zhang <yufeiGitHub@gmail.com>
2016-09-02 00:52:56 +1000

    add quiz4 solution and a test for set for fun

--- Labs/Lab5/most_popular_name.py
+++ Labs/Lab5/most_popular_name.py
@@ -5,9 +5,9 @@ import io
 for filename in os.listdir('names/'):
 	#print(filename)
 	with io.open('names/' + filename, encoding='utf-8') as ori_txt:
-		print(ori_txt)
 		#print(ori_txt)
-		#for line in ori_txt.encoding:
-		#	print(line.encoding)
+		#print(ori_txt)
+		for line in ori_txt:
+			print(line)
 	#txt = open('names/' + filename)
 	#print(txt)
---
+++ for_fun/set.py
@@ -0,0 +1,11 @@
+a = set('abracadabra')
+b = set('alacazam')
+
+eval()
+
+print(a)
+print(b)
+print(a - b)
+print(a | b)
+print(a & b)
+print(a ^ b)
