7be586e5fdb98f0bd5a6b52bae785dba6bbe6740
Yufei Zhang <yufeiGitHub@gmail.com>
2016-09-04 20:32:43 +1000

    q1 is done

--- Preparing/deficient.py
+++ Preparing/deficient.py
@@ -20,10 +20,18 @@ def main():
 			raise ValueError
 		a,b = int(a),int(b)
 
+		out = []; current = []
 		if a <= b:
-			out = []
-			current = []
 			for i in range(a, b+1):
+				if is_deficient(i):
+					current.append(i)
+				else:
+					out.append(current)
+					current = []
+			out.append(current)
+		else:
+			for i in range(a, b-1, -1):
+				print(i)
 				if is_deficient(i):
 					current.append(i)
 				else:
@@ -33,12 +41,14 @@ def main():
 
 		max_length = 0
 		max_index = -1
-		for ch in out:
-			if len(ch) > max_length:
-				max_length = len(ch)
-				max_index += 1
-
-		print("The longest sequence of deficient numbers between {:d} and {:d} ranges between {:d} and {:d}.".format(a,b,out[max_index][0],out[max_index][-1]))
+		for j in range(len(out)):
+			if len(out[j]) > max_length:
+				max_length = len(out[j])
+				max_index = j
+		if max_length > 0:
+			print("The longest sequence of deficient numbers between {:d} and {:d} ranges between {:d} and {:d}.".format(a,b,out[max_index][0],out[max_index][-1]))
+		else:
+			print("There is no deficient number between {:d} and {:d}.".format(a,b))
 	except ValueError:
 		print("Incorrect input, giving up.")
 
