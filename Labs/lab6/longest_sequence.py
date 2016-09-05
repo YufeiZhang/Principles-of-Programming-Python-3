a314383c905f32aa77f1f5a5f9b6f159c2e48bd1
Yufei Zhang <yufeiGitHub@gmail.com>
2016-09-05 16:58:27 +1000

    lab 6 q3 done

--- Labs/lab6/longest_sequence.py
+++ Labs/lab6/longest_sequence.py
@@ -11,3 +11,35 @@ $ python3 longest_sequence.py
 Please input a string of lowercase letters: ablccmdnneofffpg
 The solution is: abcdefg
 '''
+
+letters = str(input("Please input a string of lowercase letters: "))
+
+solutions = [[letters[0]]]
+
+
+for ch in letters[1:]:
+	flag = 1
+	for i in range(len(solutions)):
+		if (ord(ch.lower())-97)%26 - 1 == (ord(solutions[i][-1].lower())-97)%26:
+			solutions[i].append(ch)
+			flag = 0
+			break
+	if flag:
+		solutions.append([ch])
+
+
+max_len = 0
+max_idx = 0
+for i in range(len(solutions)):
+	if len(solutions[i]) > max_len:
+		max_len = len(solutions[i])
+		max_idx = i
+
+out = ''
+for ch in solutions[max_idx]:
+	out += ch
+
+print("The solution is:", out)
+
+
+
