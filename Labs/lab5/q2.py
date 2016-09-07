40474f72dc8977b63397a8da0c36ab9c370a9bb9
Yufei Zhang <yufeiGitHub@gmail.com>
2016-09-07 17:44:23 +1000

    clean up

--- Labs/lab5/q2.py
+++ Labs/lab5/q2.py
@@ -6,22 +6,20 @@ filename = "HNP_Data.csv"
 try:
 	file = cread(open(filename))
 
-	#indicator = str(input("Enter an indicator Name: "))
+	indicator = str(input("Enter an indicator Name: "))
 	#indicator = "Literacy rate, youth total (% of people ages 15-24)"
 	#indicator = "Age population, age 12, female, interpolated"
 	#indicator = "Newborns protected against tetanus (%)"
 	#indicator = "Number of neonatal deaths"
-	indicator = "Female headed households (% of households with a female head)"
+	#indicator = "Female headed households (% of households with a female head)"
 
 
 	next(file) # I do not need to see the first line of the csv file
-
-	max_value = 0
+	max_value = -1
 	countries = []
 
 	for line in file:
 		if line[2] == indicator:
-			#print(line)
 			for i in range(56):
 				if line[i+4] == '': num = 0
 				else:             num = float(line[i+4])
@@ -35,24 +33,24 @@ try:
 						countries.append(1960+i)
 					max_value = num
 
-	#print(countries)
-	#print(max_value)
-
 	wat = {}
 	for i in range(1, len(countries), 2):
 		if int(countries[i]) not in wat:
 			wat[int(countries[i])] = [countries[i-1]]
 		else:
 			wat[int(countries[i])].append(countries[i-1])
-	#print(wat)
 
-	print('''The maximum value is: {}\nIt was reached in these years, for these countries or categories:'''.format(round(max_value,1)))
+
+	if max_value == -1:
+		print("Sorry, either the indicator of interest does not exist or it has no data.")
+	else:
+		if max_value - int(max_value) == 0: max_value = int(max_value)
+		else: max_value = round(max_value, 1)
+		print("The maximum value is:", max_value)
+	print("It was reached in these years, for these countries or categories:")
 	for ch in wat:
 		print("\t{}: {}".format(ch, wat[ch]))
 
-
-
-	#print(file)
 
 except OSError:
 	print('There is no file named {} in the working directory, giving up...'.format(filename))
