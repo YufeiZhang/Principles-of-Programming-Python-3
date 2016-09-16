f2471185d405f834ec6aea16cda8fe6c54059508
Yufei Zhang <yufeiGitHub@gmail.com>
2016-09-16 23:01:40 +1000

    get body

--- Quizzes/quiz7/extended_linked_list.py
+++ Quizzes/quiz7/extended_linked_list.py
@@ -7,8 +7,19 @@ class ExtendedLinkedList(LinkedList):
         super().__init__(L)
 
     def rearrange(self):
-        pass
-        # replace pass above with your code
-    
+    	node = self.head
+    	#print(node.value)
+    	body = [node.value]
+    	for i in range(1, len(self)):
+    		node = node.next_node
+    		#print(node.value)
+    		body.append(node.value)
+    	print(body)
+
+
+
+
+    	pass
+
     
     
