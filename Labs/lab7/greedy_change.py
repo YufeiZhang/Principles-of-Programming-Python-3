a7e8a63afd496762b6c0600be980cf1868980449
Yufei Zhang <yufeiGitHub@gmail.com>
2016-10-26 16:03:39 +1100

    Q1 done

--- Labs/lab7/greedy_change.py
+++ Labs/lab7/greedy_change.py
@@ -1,9 +1,17 @@
 choice = [100, 50, 20, 10, 5, 2, 1]
 data = {}
-amount = input("Input the desired amount: ")
+amount = int(input("Input the desired amount: "))
 
 nowa = 0
 while amount:
-	if amount//nowa: data[choice[nowa]] = amount//nowa
+	#print(amount)
+	checking = amount//choice[nowa]
+	#print(checking)
+	if checking: data[choice[nowa]] = checking; amount -= checking*choice[nowa]
 	nowa += 1
-print(data)
+
+data = sorted(data.items(), key=lambda x:x[0], reverse = True)
+#print(data)
+
+for ch in data:
+	print('${}: {}'.format(ch[0], ch[1]))
