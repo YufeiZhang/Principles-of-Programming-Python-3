a7ebfa20467364bac9de87238ce53ec26c24ab01
Yufei Zhang <yufeiGitHub@gmail.com>
2016-11-03 19:43:42 +1100

    update

--- final/11-2.py
+++ final/11-2.py
@@ -1,16 +1,32 @@
 import sys
 from random import seed, sample
-
+from priority_queue import *
 
 def main():
-    args = input('Enter 2 nonnegative integers: ').split()
+    #args = input('Enter 2 nonnegative integers: ').split()
     try:
-        arg1 = int(args[0])
-        arg2 = int(args[1])
+        #if len(args) != 2: raise ValueError
+        #_seed  = int(args[0])
+        #length = int(args[1])
+        _seed = 0; length = 5
+        
+        if length < 0 or length > 100:
+            print('Incorrect input (2nd integer not between 1~99), give up')
+            sys.exit()        
     except ValueError:
-        print('Incorrect input (not all integers), giving up.')
+        print('Incorrect input (not all integers or not exactly two integers), giving up.')
         sys.exit()
 
+    seed(_seed)
+    L = sample(range(length * 10), length)
+    #print(L)
+    pq = PriorityQueue()
+    for e in L:
+        pq.insert(e)
+    print('The heap that has been generated is: ')
+    print(pq._data[ : len(pq) + 1])
+    print('The preferred ordering of data to generate this heap by successive insertions is:')
+    
 
 if __name__ == '__main__':
     main()
